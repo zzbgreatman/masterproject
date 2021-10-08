a5338f91776e6ba66d24213c5880397a67159dc1
ziyangliu <lzy968@gmail.com>
2021-10-08 09:14:19 -0700

    Added generate QR Code features

---
+++ app/src/main/java/com/example/dapp/CustomDialog.kt
@@ -0,0 +1,74 @@
+package com.example.dapp
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class CustomDialog(context: Context) : Dialog(context) {
+
+    @SuppressLint("InflateParams")
+    class Builder(context: Context) {
+        private var contentView: View? = null
+        private var image:Bitmap? = null
+        private var singleButtonText: String? = null
+        private var singleButtonClickListener: View.OnClickListener? = null
+
+        private val layout: View
+        private val dialog: CustomDialog = CustomDialog(context)
+
+        init {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            inflater.inflate(R.layout.custom_dialog, null).also { layout = it }
+            dialog.addContentView(layout, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        }
+
+        fun setImage(bm : Bitmap ): Builder {
+            this.image = bm
+            return this
+        }
+
+        fun setSingleButton(singleButtonText: String, listener: View.OnClickListener): Builder {
+            this.singleButtonText = singleButtonText
+            this.singleButtonClickListener = listener
+            return this
+        }
+
+        fun createSingleButtonDialog(): CustomDialog {
+            showSingleButton()
+            layout.findViewById<View>(R.id.singleBtn).setOnClickListener(singleButtonClickListener)
+            if (singleButtonText != null) {
+                (layout.findViewById<View>(R.id.singleBtn) as TextView).text = singleButtonText
+            } else {
+                (layout.findViewById<View>(R.id.singleBtn) as TextView).text = "OK"
+            }
+            create()
+            return dialog
+        }
+
+        private fun create() {
+            if (contentView != null) {
+                (layout.findViewById<View>(R.id.content) as LinearLayout).removeAllViews()
+                (layout.findViewById<View>(R.id.content) as LinearLayout)
+                    .addView(contentView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+            } else if (image != null) {
+                (layout.findViewById<View>(R.id.imageView) as ImageView).setImageBitmap(image)
+            }
+            dialog.setContentView(layout)
+            dialog.setCancelable(true)
+            dialog.setCanceledOnTouchOutside(false)
+        }
+
+        private fun showSingleButton() {
+            layout.findViewById<View>(R.id.singleButtonLayout).visibility = View.VISIBLE
+            layout.findViewById<View>(R.id.twoButtonLayout).visibility = View.GONE
+        }
+
+    }
+}
--- app/src/main/java/com/example/dapp/MainActivity.kt
+++ app/src/main/java/com/example/dapp/MainActivity.kt
@@ -1,16 +1,77 @@
 package com.example.dapp
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dapp.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+
+
+@SuppressLint("StaticFieldLeak")
+private var builderForCustom: CustomDialog.Builder? = null
+private var mDialog: CustomDialog? = null
+
+fun barcodeFormatCode(content: String): Bitmap {
+    val barcode = BarcodeFormat.QR_CODE
+    val matrix = MultiFormatWriter().encode(content, barcode, 1000, 1000, null)
+    return matrix2Bitmap(matrix)
+}
+
+private fun matrix2Bitmap(matrix: BitMatrix): Bitmap {
+    val w = matrix.width
+    val h = matrix.height
+    val rawData = IntArray(w * h)
+
+    for (i in 0 until w) {
+        for (j in 0 until h) {
+            var color = Color.WHITE
+            if (matrix.get(i, j)) {
+                color = Color.BLACK
+            }
+            rawData[i + (j * w)] = color
+        }
+    }
+
+    val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+    bitmap.setPixels(rawData, 0, w, 0, 0, w, h)
+    return bitmap
+}
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     fun startConnection(view: android.view.View) {
-        Toast.makeText(this,"Generated Deeplink: ", Toast.LENGTH_SHORT).show()
+        var sessionId = "987654321"
+        var secret = "HiMyNameIsZiyang"
+        var url = "www.walletlink.org"
+        var userId = "20616604"
+        var metadata = "127.0.0.1"
+        var deeplink = "https://${url}/userId=${userId}/secret=${secret}/sessionId=${sessionId}/metadata=${metadata}"
+        var testlink = "wc:bc61ec1b-1149-45ef-926d-a5442c60fb60@1?bridge=wss%3A%2F%2Fapi.dydx.exchange%2Fwc%2F&key=650cd910a852c946ff75cd97479492f3f64010dc1258fcf550dce46eb3021ef7"
+        builderForCustom = CustomDialog.Builder(this)
+        showSingleButtonDialog(testlink, "Cancel") {
+            mDialog!!.dismiss()
+        }
+    }
+
+    private fun showSingleButtonDialog(link:String, btnText: String, onClickListener:View.OnClickListener) {
+        mDialog = builderForCustom!!
+            .setSingleButton(btnText, onClickListener).setImage(barcodeFormatCode(link))
+            .createSingleButtonDialog()
+        mDialog!!.show()
     }
 }
